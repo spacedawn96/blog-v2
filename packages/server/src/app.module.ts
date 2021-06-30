d96959d70c924537d5aa72b100f611a98e7d924b
spacedawn96 <spacedawn96@gmail.com>
2021-07-01 04:34:40 +0900

    update

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -5,22 +5,23 @@ import { AuthModule } from './modules/auth/auth.module';
 import { UserModule } from './modules/user/user.module';
 import { User } from './modules/user/user.entity';
 
-const { file: envFilePath } = require('../../../config/env');
+// const { file: envFilePath } = require('../../../config/env');
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true, envFilePath: [envFilePath] }),
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: [] }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: async (configService: ConfigService) => ({
         type: 'mysql',
         entities: [User],
-        host: configService.get('DB_HOST', 'localhost'),
-        port: configService.get<number>('DB_PORT', 3306),
-        username: configService.get('DB_USER', 'root'),
-        password: configService.get('DB_PASSWD', '1245'),
-        database: configService.get('DB_DATABASE', 'blog'),
+        // host: configService.get('DB_HOST', 'localhost'),
+        // port: configService.get<number>('DB_PORT', 3306),
+        // username: configService.get('DB_USER', 'root'),
+        // password: configService.get('DB_PASSWD', '1245'),
+        // database: configService.get('DB_DATABASE', 'blog'),
+        url: process.env.CLEARDB_DATABASE_URL,
         charset: 'utf8mb4',
         timezone: '+08:00',
         synchronize: true,
